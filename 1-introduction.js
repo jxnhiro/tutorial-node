57d237b121d7028ed9eeafa1cf132cfef13b228a
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-15 19:49:56 +0700

    2nd Chapter - Global Variables

---
+++ 1-introduction.js
@@ -0,0 +1,9 @@
+const amount = 12
+
+if (amount < 10){
+    console.log('Small Number');
+} else {
+    console.log("Large Number");
+}
+
+console.log("Hey It's my First Node App")
---
+++ 2-globals.js
@@ -0,0 +1,9 @@
+// TODO: Global variables
+
+// __dirname - path to current directory
+// __filename - filename
+// require - function to use modules (CommonJS)
+// module - info about current module
+// process - info about environment where the program is being executed
+
+console.log(__filename);
