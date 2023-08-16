ed750b5ceec6342091f1ddfa5a300989d332f4da
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-16 15:41:21 +0700

    2nd Chapter - Callback Function and Global Variables

--- 2-globals.js
+++ 2-globals.js
@@ -6,4 +6,10 @@
 // module - info about current module
 // process - info about environment where the program is being executed
 
-console.log(__filename);
+
+//setInterval needs callback function, and the delay time in milliseconds.
+setInterval(() => {
+    console.log("Hello world.")
+}, 1000);
+
+//You can run the command with 'node 2-globals instead of node 2-globals.js with node.js
