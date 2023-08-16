bca9c47e19880210b7b68c91744a24ec1ea21292
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-16 15:45:32 +0700

    3rd Chapter - Introduction to Modules

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+//Modules in Node.JS uses CommonJS
+
+//Modules are encapsulated code (only share minimum)
+const john = "John"
+
+const sayHi = (name)=>{
+    console.log(`Hello name ${name}`);
+}
+
+sayHi('Susan');
+sayHi("John");
