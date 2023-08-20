e92f36bf62609c98256ce6e6d7317ad7d6dd025d
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-21 05:54:41 +0700

    10th Chapter - NPM Install

--- 13-npm.js
+++ 13-npm.js
@@ -1,10 +1,22 @@
-// npm - global command, comes with node
-// npm --version
+/**
+ * npm - global command, comes with node
+ * npm --version
+ */
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+/**
+ * Local Dependency - Use it only in this particular project.
+ * npm i <packageName>
+ */
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac
+/**
+ * Global Dependency - Use it in any project.
+ * npm install -g <packageName>
+ * sudo npm install -g <packageName> (mac)
+ */
 
+/**
+ *  Package.json - manifest file (stores important info abotu project/package)
+ *  1. Manual Approach (Create package.json in the root, create properties etc)
+ *  2. 'npm init' (step by step, press enter to skip)
+ *  3. 'npm init -y' (everything default)
+ */
