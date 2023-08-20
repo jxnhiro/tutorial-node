a6f884935236a4fef67d69a5948868036ca6f66b
Kenny Jinhiro <83710171+jxnhiro@users.noreply.github.com>
2023-08-20 17:36:04 +0700

    7th Chapter - FS Async Module and Callback Hell

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 const { readFile, writeFile } = require('fs');
 
 //After an event is done, we run a call back function.
+
+//Callback Hell
+readFile(
+    './content/first.txt', 
+    'utf-8',
+    (err, result)=>{
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            const first = result;
+            readFile(
+                './content/second.txt',
+                'utf-8',
+                (err, result) => {
+                    if (err){
+                        console.log(err);
+                        return;
+                    } else {
+                        const second = result;
+                        writeFile(
+                            './content/result-async.txt',
+                            `Here is the result: ${first}, ${second}`,
+                            (err, result) => {
+                                if (err){
+                                    console.log(err);
+                                } else {
+                                    console.log(result);
+                                }
+                            }
+                        )
+                    }
+                }
+            )
+        }
+    }
+);
